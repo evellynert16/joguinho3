984433cdf863f15f61b0d440a9bc2a441d574027
evellyn <108153487+evellynert16@users.noreply.github.com>
2024-08-29 15:16:08 -0300

    yg

--- js/perguntas.js
+++ js/perguntas.js
@@ -39,7 +39,7 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "se ele recusar ajudar, proxima" 
+        enunciado: "..."
         alternativas: [
             {
                 texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
